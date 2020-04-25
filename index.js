ae1738eaafa8e997f476f73c985015bd56053798
renewabristol <thomas.a.sain@gmail.com>
2020-04-25 21:54:06 +0100

    bot responds to message

--- index.js
+++ index.js
@@ -21,3 +21,43 @@ bot.on('start', () => {
       params
   );
 })
+
+bot.on('error', (err) => {
+  console.log(err);
+})
+
+const poems = [
+  {
+    line1: 'line1',
+    line2: 'line2',
+    line3: 'line3'
+  },
+  {
+    line1: 'line4',
+    line2: 'line5',
+    line3: 'line6'
+  },
+  {
+    line1: 'line7',
+    line2: 'line8',
+    line3: 'line9'
+  },
+]
+
+const sendHaiku = () => {
+  bot.postMessageToChannel(
+    'tomtest',
+    `:zap: ${poems[1].line2}`
+);
+}
+
+function handleMessage(message) {
+  if (message.includes('recite')) return sendHaiku()
+}
+
+bot.on('message', (data) => {
+  if(data.type !== 'message') {
+      return;
+  }
+  handleMessage(data.text);
+})
