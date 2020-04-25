cddf1e4402486652113a423c5efe633659a0b723
renewabristol <thomas.a.sain@gmail.com>
2020-04-25 21:48:07 +0100

    test poems

---
+++ poems.js
@@ -0,0 +1,19 @@
+const poems = [
+  {
+    line1: 'line1',
+    line2: 'line2',
+    line3: 'line3'
+  },
+  {
+    line1: 'line4',
+    line2: 'line5',
+    line3: 'line6'
+  },
+  {
+    line1: 'line7',
+    line2: 'line8',
+    line3: 'line9'
+  },
+]
+
+export default poems
