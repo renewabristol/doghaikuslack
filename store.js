029112154c44f6ae2058a0d0c1baf417c9c26a69
Tom Lawson <tom@brightanalytics.com>
2021-07-05 02:09:01 +0100

    simplify the store

--- store.js
+++ store.js
@@ -5,31 +5,17 @@ let users = {}
 let me = undefined
 let defaultChannel = undefined
 
-exports.getMessages = () => {
-  return messages
-}
+// exports.getMessages = () => messages
 
-exports.addUser = (user) => {
-  users[user.user] = user
-}
+exports.addUser = (user) => users[user.user] = user
 
-exports.getUser = (id) => {
-  return users[id]
-}
+exports.getUser = (id) => users[id]
 
-exports.setChannel = (channel) => {
-  defaultChannel = channel
-}
+// exports.setChannel = (channel) => defaultChannel = channel
 
-exports.getChannel = () => {
-  return defaultChannel
-}
+// exports.getChannel = () => defaultChannel
 
-exports.setMe= (id) => {
-  me = id
-}
+// exports.setMe = (id) => me = id
 
-exports.getMe= () => {
-  return me
-}
+// exports.getMe = () => me
 
